374cfdd8ea312dc09b8dcba8e313e349fe4bd33c
Moeed Shah <moeedshah.dev@gmail.com>
2026-09-17 18:39:43 +0500

    feat: integrate ScrollExpand kinetic component from React Bits

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
+import ScrollExpandShowcase from "./components/ScrollExpandShowcase";
 import Services from "./components/Services";
 import Learning from "./components/Learning";
 import WhyWorkWithMe from "./components/WhyWorkWithMe";
@@ -21,6 +22,7 @@ function PortfolioHome() {
         <About />
         <Skills />
         <Projects />
+        <ScrollExpandShowcase />
         <Services />
         <Learning />
         <WhyWorkWithMe />
